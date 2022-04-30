d8af4970b45e844502e9edf152e005ce3da634dd
jaeyoungchang5 <58242673+jaeyoungchang5@users.noreply.github.com>
2022-04-29 22:04:58 -0400

    restructure filter

--- golfers/src/pages/search/SearchPage.tsx
+++ golfers/src/pages/search/SearchPage.tsx
@@ -5,16 +5,19 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { MaterialIcons } from '@expo/vector-icons';
 import { Input } from 'native-base';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import ButtonToggleGroup from 'react-native-button-toggle-group';
 
 // internal imports
 import { SearchAll, SearchCourses, SearchGolfers, SearchTeeTimes } from '../../components';
 import { getCurrentLocation } from '../../utils';
 import { ICourse, IInitialCoordinates, ICoordinates } from '../../interfaces';
+import { primary_color, white } from '../../options.json';
 
 export function SearchPage() {
 	const [focused, setFocused] = useState<boolean>(false);
 	const [searchText, setSearchText] = useState<string>('');
 	const [searchTrig, setSearchTrig] = useState<boolean>(false);
+	const [searchFilter, setSearchFilter] = useState<string>('All');
     const [initialCoordinates, setInitialCoordinates] = useState<IInitialCoordinates>();
 
 	const SearchOptionTab = createMaterialTopTabNavigator();
@@ -109,7 +112,18 @@ export function SearchPage() {
 				</View>
 				<TouchableWithoutFeedback onPress={unfocusSearch}>
 					<View style={styles.content}>
-						<SearchOptionTab.Navigator
+						<ButtonToggleGroup
+							highlightBackgroundColor={primary_color}
+							highlightTextColor={white}
+							inactiveBackgroundColor={'transparent'}
+							inactiveTextColor={'grey'}
+							values={['All', 'Golfers', 'Courses', 'Tee Times']}
+							value={searchFilter}
+							onSelect={val => setSearchFilter(val)}
+							style={styles.optionBar}
+							textStyle={styles.optionText}
+						/>
+						{/* <SearchOptionTab.Navigator
 							screenOptions={{
 								tabBarLabelStyle: {fontSize: 12, textTransform: 'none'},
 								tabBarStyle: styles.optionBarStyle,
@@ -122,7 +136,7 @@ export function SearchPage() {
 							<SearchOptionTab.Screen name="Golfers" component={GolfersTab} />
 							<SearchOptionTab.Screen name="Courses" component={CoursesTab} />
 							<SearchOptionTab.Screen name="Tee Times" component={TeeTimesTab} />
-						</SearchOptionTab.Navigator>
+						</SearchOptionTab.Navigator> */}
 					</View>
 				</TouchableWithoutFeedback>
 			</KeyboardAvoidingView>
@@ -164,5 +178,15 @@ const styles = StyleSheet.create({
 		backgroundColor: 'powderblue', 
 		height: '100%', 
 		borderRadius: 4,
+	},
+	optionBar: {
+		marginTop: 10,
+		backgroundColor: white,
+		borderRadius: 4,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	optionText: {
+		fontSize: 11
 	}
 });
