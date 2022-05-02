6619c33b861a674c062130618ea604fc5f2cee8f
andrew-rocks <arocks@nd.edu>
2022-05-02 12:46:11 -0400

    getTeetimes complete

--- backend/flask-server/sas/teetime.py
+++ backend/flask-server/sas/teetime.py
@@ -15,23 +15,43 @@ bp = Blueprint('teetime', __name__, url_prefix='/teetime')
 @bp.route('/getTeetimes', methods=('GET', 'POST'))
 def getTeetimes():
     if request.method == 'POST':
+        course_id = request.form["course_id"]
+        tt_date = request.form["tt_date"]
         db = get_db()
         cursor = db.cursor()
 
+        data = dict(cid=course_id, dat=tt_date)
         query = """
             SELECT * 
             FROM teetime
+            WHERE tt_date = :dat and course_id = :cid
+            ORDER BY tt_time
         """
 
         res = cursor.execute(
-            query
+            query,
+            data
         ).fetchall()
 
-        if res is not None:
-            print(res)
-            return res
+        # iterate over teetimes from result of query
+        rowdata = []
+        for row in res:
+            data = {}
+            column_names = [i[0] for i in cursor.description]
+            for i in range(len(column_names)):
+                data.update({str(column_names[i]) : row[i]})
+            rowdata.append(data)
+
+        returnjson = {}
+        returnjson.update({'teetimes' : rowdata})
+
+
+        if len(res) != 0:
+            returnjson.update({'success' : True})
+            return returnjson
         else:
-            return 'Invalid'
+            returnjson.update({'success' : False, 'message' : 'no teetimes found'})
+            return returnjson
 
 
 @bp.route('/uploadTeetime', methods=('GET', 'POST'))
@@ -45,8 +65,8 @@ def uploadTeetime():
 
         data = dict(cid=course_id, tim=tt_time, dat=tt_date)
         query = """
-            INSERT INTO teetime (course_id, tt_time, tt_date)
-            VALUES (:cid, :tim, :dat)
+            INSERT INTO teetime (course_id, golfer1_id, golfer2_id, golfer3_id, golfer4_id, tt_time, tt_date)
+            VALUES (:cid, NULL, NULL, NULL, NULL, :tim, :dat)
         """
 
         try:
